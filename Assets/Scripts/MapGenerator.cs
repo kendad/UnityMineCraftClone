c722872badd557a15758e2616de377e948fb2dc4
KAUSHIK ISWARY <kaushikiswary@gmail.com>
2022-06-20 07:26:39 +0530

    proper rendering

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -6,8 +6,11 @@ public class MapGenerator : MonoBehaviour
 {
     List<GameObject> cubes=new List<GameObject> ();
 
-    int xSize = 20;
-    int zSize = 20;
+    static int xSize = 40;
+    static int zSize = 40;
+
+    GameObject[,,] cubesArray = new GameObject[xSize,10,zSize];
+    float[,] perlinNoiseDistribution = new float[xSize, zSize];
 
     float height = 0.1f;
 
@@ -24,6 +27,7 @@ public class MapGenerator : MonoBehaviour
             for(int z = 0; z < zSize; z++)
             {
                 float perlinNoise=Mathf.PerlinNoise(x*height,z*height)*10;
+                perlinNoiseDistribution[x, z] = perlinNoise;
                 for(int y = 0; y < perlinNoise; y++)
                 {
                     GenerateCubes(x,y,z);
@@ -49,102 +53,66 @@ public class MapGenerator : MonoBehaviour
         cube.transform.position+=new Vector3(x,y,z);
 
         cubes.Add(cube);
+        cubesArray[x, y, z] = cube;
     }
 
     void UpdateCube()
     {
-        /*foreach(GameObject cube in cubes)
+        for(int x = 0; x < xSize; x++)
         {
-            Vector3 positionUp = new Vector3(cube.transform.position.x, cube.transform.position.y+1, cube.transform.position.z);
-            bool isTop = CheckIfCubeExists(positionUp);
-
-            Vector3 positionDown = new Vector3(cube.transform.position.x, cube.transform.position.y-1, cube.transform.position.z);
-            bool isBottom=CheckIfCubeExists(positionDown);
-
-
-
-            Vector3 positionLeft = new Vector3(cube.transform.position.x, cube.transform.position.y, cube.transform.position.z - 1);
-            bool isLeft=CheckIfCubeExists(positionLeft);
-
-            Vector3 positionRight = new Vector3(cube.transform.position.x, cube.transform.position.y, cube.transform.position.z + 1);
-            bool isRight = CheckIfCubeExists(positionRight);
-
-
-
-            Vector3 positionFront = new Vector3(cube.transform.position.x + 1, cube.transform.position.y, cube.transform.position.z);
-            bool isFront = CheckIfCubeExists(positionFront);
-
-            Vector3 positionBack = new Vector3(cube.transform.position.x - 1, cube.transform.position.y, cube.transform.position.z);
-            bool isBack=CheckIfCubeExists(positionBack);
-
-         
-            MeshData meshData = new MeshData();
-
-            Mesh mesh = new Mesh();
-            mesh.vertices = meshData.vertices.ToArray();
-            meshData.UpdateTriangleData(isTop, isBottom, isRight, isLeft, isFront, isBack);
-            mesh.triangles = meshData.triangles.ToArray();
-            mesh.RecalculateNormals();
-            cube.GetComponent<MeshFilter>().mesh = mesh;
-
-        }*/
-
-        for(int i = 0; i < cubes.Count; i++)
-        {
-            Vector3 positionUp = new Vector3(cubes[i].transform.position.x, cubes[i].transform.position.y + 1, cubes[i].transform.position.z);
-            bool isTop = CheckIfCubeExists(positionUp,i+1);
-
-            Vector3 positionDown = new Vector3(cubes[i].transform.position.x, cubes[i].transform.position.y - 1, cubes[i].transform.position.z);
-            bool isBottom = CheckIfCubeExists(positionDown,i-1);
-
-
-
-            Vector3 positionLeft = new Vector3(cubes[i].transform.position.x, cubes[i].transform.position.y, cubes[i].transform.position.z - 1);
-            bool isLeft = CheckIfCubeExists(positionLeft);
-
-            Vector3 positionRight = new Vector3(cubes[i].transform.position.x, cubes[i].transform.position.y, cubes[i].transform.position.z + 1);
-            bool isRight = CheckIfCubeExists(positionRight);
-
-
-
-            Vector3 positionFront = new Vector3(cubes[i].transform.position.x + 1, cubes[i].transform.position.y, cubes[i].transform.position.z);
-            bool isFront = CheckIfCubeExists(positionFront);
-
-            Vector3 positionBack = new Vector3(cubes[i].transform.position.x - 1, cubes[i].transform.position.y, cubes[i].transform.position.z);
-            bool isBack = CheckIfCubeExists(positionBack);
-
-
-            MeshData meshData = new MeshData();
-
-            Mesh mesh = new Mesh();
-            mesh.vertices = meshData.vertices.ToArray();
-            meshData.UpdateTriangleData(isTop, isBottom, isRight, isLeft, isFront, isBack);
-            mesh.triangles = meshData.triangles.ToArray();
-            mesh.RecalculateNormals();
-            cubes[i].GetComponent<MeshFilter>().mesh = mesh;
-        }
-    }
-
-    bool CheckIfCubeExists(Vector3 direction)
-    {
-        foreach(GameObject cube in cubes)
-        {
-            if (cube.transform.position == direction)
+            for(int z = 0; z < zSize; z++)
             {
-                return true;
+                float perlinNoise = perlinNoiseDistribution[x, z];
+                for (int y = 0; y < perlinNoise; y++)
+                {
+                    Vector3 positionUp = new Vector3(cubesArray[x,y,z].transform.position.x, cubesArray[x,y,z].transform.position.y + 1, cubesArray[x,y,z].transform.position.z);
+                    bool isTop = CheckIfCubeExists(positionUp, x, y + 1, z, perlinNoise);
+
+                    Vector3 positionDown = new Vector3(cubesArray[x,y,z].transform.position.x, cubesArray[x,y,z].transform.position.y - 1, cubesArray[x,y,z].transform.position.z);
+                    bool isBottom = CheckIfCubeExists(positionDown, x, y - 1, z, perlinNoise);
+
+
+
+                    Vector3 positionLeft = new Vector3(cubesArray[x,y,z].transform.position.x, cubesArray[x,y,z].transform.position.y, cubesArray[x,y,z].transform.position.z - 1);
+                    bool isLeft = CheckIfCubeExists(positionLeft, x, y, z - 1, perlinNoise);
+
+                    Vector3 positionRight = new Vector3(cubesArray[x,y,z].transform.position.x, cubesArray[x,y,z].transform.position.y, cubesArray[x,y,z].transform.position.z + 1);
+                    bool isRight = CheckIfCubeExists(positionRight, x, y, z + 1, perlinNoise);
+
+
+
+                    Vector3 positionFront = new Vector3(cubesArray[x,y,z].transform.position.x + 1, cubesArray[x,y,z].transform.position.y, cubesArray[x,y,z].transform.position.z);
+                    bool isFront = CheckIfCubeExists(positionFront, x + 1, y, z, perlinNoise);
+
+                    Vector3 positionBack = new Vector3(cubesArray[x,y,z].transform.position.x - 1, cubesArray[x,y,z].transform.position.y, cubesArray[x,y,z].transform.position.z);
+                    bool isBack = CheckIfCubeExists(positionBack, x - 1, y, z, perlinNoise);
+
+
+                    MeshData meshData = new MeshData();
+
+                    Mesh mesh = new Mesh();
+                    mesh.vertices = meshData.vertices.ToArray();
+                    meshData.UpdateTriangleData(isTop, isBottom, isRight, isLeft, isFront, isBack);
+                    mesh.triangles = meshData.triangles.ToArray();
+                    mesh.RecalculateNormals();
+                    cubesArray[x,y,z].GetComponent<MeshFilter>().mesh = mesh;
+                }
             }
         }
-        return false;
     }
 
-    bool CheckIfCubeExists(Vector3 direction,int index)
+    bool CheckIfCubeExists(Vector3 direction,int x,int y,int z,float perlinNoise)
     {
-        if (index>=0 && index < cubes.Count)
+        try
         {
-            Debug.Log(index);
-            Debug.Log(direction);
-            Debug.Log("###");
-            if (cubes[index].transform.position == direction) { return true; }
+            if (y >= 0 && y < perlinNoise && x >= 0 && x < xSize && z >= 0 && z < zSize)
+            {
+                if (cubesArray[x, y, z].transform.position == direction) { return true; }
+            }
+        }
+        catch
+        {
+            return false;
         }
         return false;
     }
